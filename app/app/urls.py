3899e43df592d9c700c9db8aff8ee018c6fcb9e3
sergeisidorkin <sidorkin.sergei@gmail.com>
2024-03-18 14:52:27 +0300

    An index file has been created

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
+from main import views
+
+urlpatterns: list = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here
+def index(request) -> HttpResponse:
+    context: dict[str, Any] = {
+        'title': 'Home',
+        'content': 'Главная страница магазина',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'bool': True
+    }
+    return render(request,'main/index.html', context)
+def about(request) -> HttpResponse:
+    return HttpResponse('About page')
