d06b1d0b85ef8b4349a7a0429a078e2d78f8ecbe
sergeisidorkin <sidorkin.sergei@gmail.com>
2024-04-07 23:43:02 +0300

    Slug converter for product

--- app/app/urls.py
+++ app/app/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
-    path('catalog', include('goods.urls', namespace='catalog')),
+    path('catalog/', include('goods.urls', namespace='catalog')),
 ]
 
 if settings.DEBUG:
