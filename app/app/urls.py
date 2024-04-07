6da80f77f171a9a585e46f63af9ae1e2043ea080
sergeisidorkin <sidorkin.sergei@gmail.com>
2024-04-07 21:15:49 +0300

    Registered simple tag for categories

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,10 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from app import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('catalog', include('goods.urls', namespace='catalog')),
-
-    path("__debug__/", include("debug_toolbar.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
