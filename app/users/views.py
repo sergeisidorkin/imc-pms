ec4e896bd898887f73ff705f4713065df00e6470
sergeisidorkin <sidorkin.sergei@gmail.com>
2024-04-13 20:50:19 +0300

    Login form and controller are ready

--- app/users/views.py
+++ app/users/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from typing import List, Any, Dict
 
 
-def login(request):
+from users.forms import UserLoginForm
+
+
+def login(request) -> HttpResponseRedirect:
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+               auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
     context = {
         'title': 'Home - Авторизация',
+        'form': form
     }
     return render(request, 'users/login.html', context)
+
 
 def registration(request):
     context = {
@@ -13,11 +35,13 @@ def registration(request):
     }
     return render(request, 'users/registration.html', context)
 
+
 def profile(request):
     context = {
         'title': 'Home - Кабинет',
     }
     return render(request, 'users/profile.html', context)
+
 
 def logout(request):
     context = {
